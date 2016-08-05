7c6c644b6a4952cd2b5b95b46127c034afcf15a5
Alexander zowers Petrov <apetrov@meta-comm.com>
2016-08-05 13:41:45 +0300

    fixed ariya/phantomjs#14452 PDF Standard max page size of 14400pt is not enforced

--- src/webpage.cpp
+++ src/webpage.cpp
@@ -1174,8 +1174,8 @@ bool WebPage::renderPdf(const QString& fileName)
     }
 
     if (paperSize.contains("width") && paperSize.contains("height")) {
-        const QSizeF sizePt(ceil(stringToPointSize(paperSize.value("width").toString())),
-                            ceil(stringToPointSize(paperSize.value("height").toString())));
+        const QSizeF sizePt(fmin(14400.0, ceil(stringToPointSize(paperSize.value("width").toString()))),
+                            fmin(14400.0, ceil(stringToPointSize(paperSize.value("height").toString()))));
         printer.setPaperSize(sizePt, QPrinter::Point);
     } else if (paperSize.contains("format")) {
         const QPrinter::Orientation orientation = paperSize.contains("orientation")
